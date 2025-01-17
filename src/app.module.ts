1869de71768eac0c9b3f4d19cdf44501312d1c12
dinamuh <dinamuhammed2017@gmail.com>
2025-01-17 14:58:03 +0200

    starting the components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { BookService } from './book/book.service';
+import { RecommendationService } from './recommendation/recommendation.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your-username',
+      password: 'your-password',
+      database: 'reading_recommendation',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, BookService, RecommendationService],
 })
 export class AppModule {}
